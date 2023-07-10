395e2d9acea2829aa395f970221e6cc42a7172a2
Ankit Rajput <113281225+ankitrajput77@users.noreply.github.com>
2023-07-10 17:08:22 +0530

    info function update

--- components/utils.py
+++ components/utils.py
@@ -166,7 +166,7 @@ def about_me():
         st.title("About the Creator")
         c1, c2 = st.columns([1,1])
         c1.markdown("""Hey! My name is **Ankit Rajput**, Mathematics and Computing student in Indian Institute Of Technology, Guwahati.
-                   c1.markdown() This app is for Whatsapp chat Analysis, I am updating this app continuously.
+                    This app is for Whatsapp chat Analysis, I am updating this app continuously.
                     If you have any Questions or Suggestion about this app and you want to discuss it with me
                     Contact me on rajputankit72106@gmail.com""")
         c1.markdown("If you are interested :")
